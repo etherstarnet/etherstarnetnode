1cf225de44a9294c265b65104c6574b60a5aad28
etherstarnet <etherstarnet@gmail.com>
2025-08-20 20:01:30 -0400

    Initial code commit

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -39,7 +39,7 @@ import (
 
 // Ethash proof-of-work protocol constants.
 var (
-	FrontierBlockReward           = big.NewInt(10e+18) // Block reward in wei for successfully mining a block
+	FrontierBlockReward           = big.NewInt(9e+18) // Block reward in wei for successfully mining a block
 	ByzantiumBlockReward          = big.NewInt(5e+18) // Block reward in wei for successfully mining a block upward from Byzantium
 	ConstantinopleBlockReward     = big.NewInt(3e+18) // Block reward in wei for successfully mining a block upward from Constantinople
 	maxUncles                     = 2                 // Maximum number of uncles allowed in a single block
